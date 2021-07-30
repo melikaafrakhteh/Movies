dba86dfd2a11426d4a34b535542b84e31b0b0dd5
melikaafrakhteh <45101381+melikaafrakhteh@users.noreply.github.com>
2021-07-30 21:11:36 +0430

    implement forget password

--- app/src/main/java/com/afrakhteh/movies/util/consts/URLS.kt
+++ app/src/main/java/com/afrakhteh/movies/util/consts/URLS.kt
@@ -4,7 +4,7 @@ class URLS {
 
     companion object {
 
-    const val BASE_URL = "http://localhost:8080/movie/"
+     const val BASE_URL = "http://localhost:8080/movie/"
    //   const val BASE_URL = "http://10.0.2.2/movie/"
 
 
